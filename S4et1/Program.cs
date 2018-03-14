cc64ba358b9980db66672d8a77bb4cc12b541265
GTXZ <mazafaka_1999_1@mail.ru>
2018-03-14 08:49:03 +0300

    Prog

--- S4et1/Program.cs
+++ S4et1/Program.cs
@@ -4,12 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace S4et1
 {
-    class Program
+    class S4et1
     {
+        public String Name { get; set; }
+
+        private int nomerS4eta;
+        public int Num { get; set; }
+
+        public double Summa { get; set; }
+
+        public S4et1()
+        {
+            this.Num = 0;
+            this.nomerS4eta = 0;
+            this.Summa = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введите наименование счета: "); this.Name = Console.ReadLine();
+            Console.Write("Введите номер счета: "); this.nomerS4eta = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сумму счета: "); this.Summa = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Наименнование: { this.Name }");
+            Console.WriteLine($"Номер счета: { this.nomerS4eta }");
+            Console.WriteLine($"Сумма счета: { this.Summa }");
+        }
+
         static void Main(string[] args)
         {
+            S4et1 scr = new S4et1();
+            scr.Input();
+            scr.Print();
+            Console.ReadKey();
         }
+
     }
 }
